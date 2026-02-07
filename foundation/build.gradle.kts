b94733a5c78b9c191b858b81af8c62d4589988df
lsassetmng@gmail.com <lsassetmng@gmail.com>
2026-02-07 23:03:47 +0900

    test

---
+++ foundation/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.lsam.gameplatform.foundation"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+dependencies {
+    // Phase2: no dependencies
+}
---
+++ foundation/src/main/java/com/lsam/gameplatform/foundation/core/update/UpdateResult.java
@@ -0,0 +1,7 @@
+package com.lsam.gameplatform.foundation.core.update;
+
+public class UpdateResult {
+    public boolean updateRequired;
+    public boolean force;
+    public String storeUrl;
+}
